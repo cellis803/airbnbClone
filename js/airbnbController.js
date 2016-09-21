06eb0b758c0d8ef92897d5e751bb489b8ff1974e
akashpandya1 <akashpandya@gmail.com>
2016-09-21 15:28:39 -0400

    added filter on page.

--- js/airbnbController.js
+++ js/airbnbController.js
@@ -2,8 +2,16 @@
 var app = angular.module('airbnbApp', []);
  
 app.controller('airbnbCtrl', function($http, $scope, airbnbFactory) {
-    $scope.airbnblistings;
+     
+     $scope.airbnblistings;
 
+     $scope.newListing = {};   
+
+     $scope.rentalAmt = {
+      min: 0,
+      max: 1000
+    } 
+    
     airbnbFactory.getListings().success(function(data) {
          $scope.airbnblistings = data;
     }).error(function(error) {
