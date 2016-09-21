136115313eda05564208c2fd4cfed55dbd0cfa8c
cellis803 <cellis803@gmail.com>
2016-09-21 14:03:08 -0400

    listings api

--- js/airbnbFactory.js
+++ js/airbnbFactory.js
@@ -4,8 +4,8 @@ var app = angular.module('airbnbApp');
  app.factory('airbnbFactory', function($http) {
 
      function getListings() {
-      return $http.get('static/data/airbnb.json');
-      //return $http.get('/listings');
+      //return $http.get('static/data/airbnb.json');
+      return $http.get('/listings');
     }
 
     return {
