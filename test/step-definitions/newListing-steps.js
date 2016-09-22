a63e0721ee0bfdf71f165385e23c56408e0c1627
cellis803 <cellis803@gmail.com>
2016-09-22 12:00:06 -0400

    testing add new listing

--- test/step-definitions/newListing-steps.js
+++ test/step-definitions/newListing-steps.js
@@ -3,15 +3,18 @@
        this.When(/^I click on Add Listing$/, function (done) {                                                                                                                                   
             
             driver.findElements(by.xpath("//button[contains(text(),'Add Listing')]")).then(function (elements) {
-                elements[0].click();
-                done(); 
+                elements[0].click().then(function() {
+                    done();
+                });
+
                                                                                                                                                                                 
             });                                                                                                                                                                                                                                                                                     
        });   
 
 
      this.Then(/^I should see the add new listing page$/, function (done) {                                                                                                                    
-            driver.findElements(by.xpath("//h3[contains(text(),'Add A Listing')]")).then(function (elements) {
+            driver.findElements(by.xpath("//h3[contains(text(),'Add a Listing')]")).then(function (elements) {
+                expect(elements.length).to.not.equal(0);         
                 done(); 
                                                                                                                                                                                 
             });                                                                                                                                                               
