4a7aca86bf8be05432f2d079ea0e848325e51d4d
cellis803 <cellis803@gmail.com>
2016-09-23 07:39:04 -0400

    test scripts

---
+++ test/cleanDB.js
@@ -0,0 +1,15 @@
+var airbnbDB = require('../db.js');
+
+console.log('Initializing DB with test data...');
+
+var p = airbnbDB.initDB();
+p.then(
+    val => {
+            airbnbDB.loadTestData().then(
+                () => {console.log("...done");} 
+            );
+
+}).catch(
+    err => {
+        console.log(err);
+});
