b068cbd762d2db0da9fdf39e36aada1793dd3fba
cellis803 <cellis803@gmail.com>
2016-09-27 16:38:42 -0400

    routing for map view

--- js/airbnbRouter.js
+++ js/airbnbRouter.js
@@ -11,7 +11,7 @@ app.config(function($routeProvider) {
         controller: 'airbnbCtrl'
     }).
     when('/mapView', {
-        templateUrl: 'listing-details.html',
+        templateUrl: 'mapView.html',
         controller: 'airbnbCtrl'
     }).
     when('/listing/:listingId', {
