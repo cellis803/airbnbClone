1f717497746a5ed4111496965b28eb7fcb048166
carolynjm4 <carolynjm4@verizon.net>
2016-09-29 14:10:47 -0400

    added pix to review screen

--- js/airbnbController.js
+++ js/airbnbController.js
@@ -11,149 +11,151 @@ app.controller('airbnbCtrl', function ($http, $scope, airbnbFactory, $routeParam
   $scope.airbnbreviews;
   $scope.newReview = {};
   $scope.reviewSelectedListing = airbnbFactory.reviewSelectedListing;
+  $scope.imageName = airbnbFactory.reviewImageName;
+
   $scope.createReview = {};
 
   $scope.rentalAmt = {
     min: 0,
     max: 100000
   }
- 
- $scope.markers = [];
-  var mapOptions = {
-      zoom: 1,
-      center: new google.maps.LatLng(-34.397, 150.644),
-      mapTypeId: google.maps.MapTypeId.TERRAIN
-  }
- 
 
- $scope.init = function() {
-  airbnbFactory.getListings().success(function (data) {
+  $scope.markers = [];
+  var mapOptions = {
+    zoom: 1,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  }
+
+
+  $scope.init = function () {
+    airbnbFactory.getListings().success(function (data) {
       $scope.airbnblistings = data;
     }).error(function (error) {
       console.log(error);
     });
- }
+  }
 
   $scope.openInfoWindow = function (e, mapListing) {
     console.log('openInfoWindow', mapListing.latitude, mapListing.logitude);
     if (e) {
       e.preventDefault();
     }
-     $scope.map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: new google.maps.LatLng(mapListing.latitude, mapListing.logitude),
-          zoomControl: true,
-          mapTypeControl: true,
-          scaleControl: true,
-          streetViewControl: true,
-          rotateControl: true,
-          fullscreenControl: true,
-          mapTypeControlOptions: {
-              style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-              mapTypeIds: ['roadmap', 'terrain']
-           }
-        }); 
+    $scope.map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: new google.maps.LatLng(mapListing.latitude, mapListing.logitude),
+      zoomControl: true,
+      mapTypeControl: true,
+      scaleControl: true,
+      streetViewControl: true,
+      rotateControl: true,
+      fullscreenControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        mapTypeIds: ['roadmap', 'terrain']
+      }
+    });
     var markerlocal = new google.maps.Marker({
-        map: $scope.map,
-        title: mapListing.title,
-        position:new google.maps.LatLng(mapListing.latitude, mapListing.logitude),
-        animation:google.maps.Animation.BOUNCE
-    });    
+      map: $scope.map,
+      title: mapListing.title,
+      position: new google.maps.LatLng(mapListing.latitude, mapListing.logitude),
+      animation: google.maps.Animation.BOUNCE
+    });
   }
 
-$scope.loadMyMap = function () {
-    var data = $scope.airbnblistings;  
-    console.log("data: ", data.length, ", " , data);  
-  
-   $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions); 
-   var infoWindow = new google.maps.InfoWindow();  
-   var address;    
-   var j= data.length;
-   
-     //loop over all addresses
-    for(var i=0; i<j; i++) {     
-      new function(){
-         var iCopy = i;            
-          address = data[iCopy].address1 + ', ' + data[iCopy].city + ', ' + data[iCopy].state  + ' ' + data[iCopy].zip;
-          console.log('address', address);         
-          // console.log('latlong: ',results[0].geometry.location.lat(), results[0].geometry.location.lng());
-          // console.log('creating Marker:', iCopy, , data[iCopy]);                   
-          var marker = new google.maps.Marker({
-                                      map: $scope.map,
-                                      position: new google.maps.LatLng(data[iCopy].latitude,data[iCopy].logitude),
-                                      title: data[iCopy].address1 + ', ' + data[iCopy].city + ', ' + data[iCopy].state  + ' ' + data[iCopy].zip,
-                                      rowid: data[iCopy].rowid                                      
-                           });
-                            console.log('marker:',   marker.title, marker.rowid); 
-                            marker.content = '<div class="infoWindowContent">' + data[iCopy].description + '</div>';
-                            google.maps.event.addListener(marker, 'click', function(){                               
-                                                infoWindow.setContent('<h2>' + data[iCopy].title + '</h2>' + marker.content);                                                        
-                                                infoWindow.open($scope.map, marker);
-                            });
-             $scope.markers.push(marker);                   
-         }       
-    }      
+  $scope.loadMyMap = function () {
+    var data = $scope.airbnblistings;
+    console.log("data: ", data.length, ", ", data);
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var infoWindow = new google.maps.InfoWindow();
+    var address;
+    var j = data.length;
+
+    //loop over all addresses
+    for (var i = 0; i < j; i++) {
+      new function () {
+        var iCopy = i;
+        address = data[iCopy].address1 + ', ' + data[iCopy].city + ', ' + data[iCopy].state + ' ' + data[iCopy].zip;
+        console.log('address', address);
+        // console.log('latlong: ',results[0].geometry.location.lat(), results[0].geometry.location.lng());
+        // console.log('creating Marker:', iCopy, , data[iCopy]);                   
+        var marker = new google.maps.Marker({
+          map: $scope.map,
+          position: new google.maps.LatLng(data[iCopy].latitude, data[iCopy].logitude),
+          title: data[iCopy].address1 + ', ' + data[iCopy].city + ', ' + data[iCopy].state + ' ' + data[iCopy].zip,
+          rowid: data[iCopy].rowid
+        });
+        console.log('marker:', marker.title, marker.rowid);
+        marker.content = '<div class="infoWindowContent">' + data[iCopy].description + '</div>';
+        google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.setContent('<h2>' + data[iCopy].title + '</h2>' + marker.content);
+          infoWindow.open($scope.map, marker);
+        });
+        $scope.markers.push(marker);
+      }
+    }
   }
 
   $scope.createListing = function (newListing) {
-    console.log('newListing', newListing);    
+    console.log('newListing', newListing);
     if (newListing) {
-      var address = address = newListing.address1 + ', ' + newListing.city + ', ' + newListing.state  + ' ' + newListing.zip;
+      var address = address = newListing.address1 + ', ' + newListing.city + ', ' + newListing.state + ' ' + newListing.zip;
       var geocoder = new google.maps.Geocoder();
-      geocoder.geocode( { 'address': address}, function(results, status) {              
-          if (status == 'OK') { 
-            newListing.latitude = results[0].geometry.location.lat();
-            newListing.logitude = results[0].geometry.location.lng(); 
-            console.log('latlag:' +  newListing.latitude,newListing.logitude);
-            console.log('newListing after Geo call:', newListing);    
-            newListing.image = newListing.image.name;
-            newListing.name = newListing.email;
-            newListing.email = airbnbFactory.user.email;
-            //console.log('newListing:' + JSON.stringify(newListing));
-            airbnbFactory.addListing(newListing).success(function (data) {
-              //console.log('rowid:' + data.rowid);
-              newListing.rowid = data.rowid;
-              $scope.airbnblistings.push(newListing);
-              //console.log('airbnblistings length:' + $scope.airbnblistings.length);
-            }).error(function (error) {
-              console.log(error);
-            });
-            $scope.newListing = {};
-          } else {             
-             console.log('Geocode was not successful for the following reason: ' + status);
-             alert('Geocode was not successful for the following reason: ' + status);
-          }      
-      });       
+      geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == 'OK') {
+          newListing.latitude = results[0].geometry.location.lat();
+          newListing.logitude = results[0].geometry.location.lng();
+          console.log('latlag:' + newListing.latitude, newListing.logitude);
+          console.log('newListing after Geo call:', newListing);
+          newListing.image = newListing.image.name;
+          newListing.name = newListing.email;
+          newListing.email = airbnbFactory.user.email;
+          //console.log('newListing:' + JSON.stringify(newListing));
+          airbnbFactory.addListing(newListing).success(function (data) {
+            //console.log('rowid:' + data.rowid);
+            newListing.rowid = data.rowid;
+            $scope.airbnblistings.push(newListing);
+            //console.log('airbnblistings length:' + $scope.airbnblistings.length);
+          }).error(function (error) {
+            console.log(error);
+          });
+          $scope.newListing = {};
+        } else {
+          console.log('Geocode was not successful for the following reason: ' + status);
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
     }
   }
 
   $scope.handleLogin = function (email) {
-    airbnbFactory.login(email).then(function(result) {
-        airbnbFactory.user = result.data;
-        $scope.loginError = false;
-        window.location = "/#/login";
-    }, function() {
-        $scope.loginError = true;
-        console.log("could not log in user");
+    airbnbFactory.login(email).then(function (result) {
+      airbnbFactory.user = result.data;
+      $scope.loginError = false;
+      window.location = "/#/login";
+    }, function () {
+      $scope.loginError = true;
+      console.log("could not log in user");
     });
 
   }
 
- 
+
   $scope.mapView = function (allListings) {
     airbnbFactory.airbnblistings = allListings;
     window.location = "/#/mapView";
-    setTimeout(function() {
+    setTimeout(function () {
       $scope.loadMyMap();
     }, 500);
   }
 
 
-  $scope.logout = function() {
-        $scope.useremail = "";
-        $scope.user = null;
-   }    
- 
+  $scope.logout = function () {
+    $scope.useremail = "";
+    $scope.user = null;
+  }
+
 
   $scope.editDetails = function (editData) {
     $scope.editListing = true;
@@ -206,58 +208,62 @@ $scope.loadMyMap = function () {
     console.log(airbnbFactory.user.email);
     newReview.email = airbnbFactory.user.email;
     airbnbFactory.createNewReview(newReview).success(function (data) {
-      
+
     }).error(function (error) {
       console.log(error);
     });
-    
+
   }
 
   $scope.cancel = function () {
-      history.back();
+    history.back();
   }
 
-    $scope.review = function (listing) {
-      airbnbFactory.reviewSelectedListing = listing;
-      console.log('$scope.reviewSelectedListing:', airbnbFactory.reviewSelectedListing);
-      window.location = "/#/listing/" + airbnbFactory.reviewSelectedListing + "/review";
-    }  
+  $scope.review = function (listing, imageName) {
+    airbnbFactory.reviewSelectedListing = listing;
+    airbnbFactory.reviewImageName = imageName;
+
+    console.log('$scope.reviewSelectedListing:', airbnbFactory.reviewSelectedListing);
+    console.log('$scope.image:', airbnbFactory.reviewimage);
+
+    window.location = "/#/listing/" + airbnbFactory.reviewSelectedListing + "/review";
+  }
 
 });
 
-app.controller('ListingDetailsCtrl', function($scope, $routeParams, airbnbFactory) {
+app.controller('ListingDetailsCtrl', function ($scope, $routeParams, airbnbFactory) {
 
-    $scope.listingReviews = {};
+  $scope.listingReviews = {};
 
-    airbnbFactory.getListingById($routeParams.listingId).then(function (result) {
-            $scope.listingDetails = result.data;
-            $scope.airbnblisting = result.data;
-            
-            setTimeout(function() {
-              $scope.openInfoWindow(null, result.data);
-            }, 1);
+  airbnbFactory.getListingById($routeParams.listingId).then(function (result) {
+    $scope.listingDetails = result.data;
+    $scope.airbnblisting = result.data;
 
-    }, function (error) {
-      console.log(error);
-    });
+    setTimeout(function () {
+      $scope.openInfoWindow(null, result.data);
+    }, 1);
 
-    $scope.convertNumberToArray = function(number) {
-      var numArray = new Array(number);
-      for (var i=0; i < number; i++) {
-        numArray[i] = i;
-      }
-      return numArray;
+  }, function (error) {
+    console.log(error);
+  });
+
+  $scope.convertNumberToArray = function (number) {
+    var numArray = new Array(number);
+    for (var i = 0; i < number; i++) {
+      numArray[i] = i;
     }
+    return numArray;
+  }
 
-    $scope.reviewRatingToConvert = [];
-    airbnbFactory.getReviewsByListing($routeParams.listingId).then(function(result) {
-            $scope.listingReviews = result.data;
-            //$scope.reviewRatingToConvert [] = result.rating [];
-            
+  $scope.reviewRatingToConvert = [];
+  airbnbFactory.getReviewsByListing($routeParams.listingId).then(function (result) {
+    $scope.listingReviews = result.data;
+    //$scope.reviewRatingToConvert [] = result.rating [];
 
-    }, function(error) {
-        console.log(error);
-    });
+
+  }, function (error) {
+    console.log(error);
+  });
 
 
 });
